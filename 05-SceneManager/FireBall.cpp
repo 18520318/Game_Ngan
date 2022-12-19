d8774aef93249a89388d285fce34b2f9218f9fb9
NganNguyen <18520318@gm.uit.edu.vn>
2022-12-20 00:54:48 +0700

    Add Fire Ball properties

--- 05-SceneManager/FireBall.cpp
+++ 05-SceneManager/FireBall.cpp
@@ -13,3 +13,28 @@ void FireBall::Render()
 	int aniId = FIREBALL_ANI;
 	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
 }
+
+void FireBall::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void FireBall::SetState(int state)
+{
+	CGameObject::SetState(state);
+	CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	mario_dir = mario->GetCurrentDirection();//Get Mario's current direction
+
+	if (state == FIRE_FROM_MARIO) {
+		vx = mario_dir * FIRE_BALL_SPEED;
+	}
+}
+
+FireBall::FireBall(float x, float y):CGameObject(x, y)
+{
+	this->x = x;
+	this->y = y;
+	this->ax = 0;
+	this->ay = FIRE_GRAVITY;
+}
