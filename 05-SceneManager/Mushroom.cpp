5dc48f4e992ac9e724577643a69d4c053900c277
NganNguyen <18520318@gm.uit.edu.vn>
2023-01-21 23:04:31 +0700

    Update Mushroom running and falling

--- 05-SceneManager/Mushroom.cpp
+++ 05-SceneManager/Mushroom.cpp
@@ -1,15 +1,13 @@
 #include "Mushroom.h"
 
-Mushroom::MushRoom(float x, float y)
-{
-}
-
 CMushroom::CMushroom(float x, float y) : CGameObject(x, y)
 {
 	this->ay = 0;
 	this->ax = 0;
 	this->x = x;
 	this->y = y;
+
+	minY = y - MUSHROOM_BBOX_HEIGHT;
 }
 
 void CMushroom::GetBoundingBox(float& left, float& top, float& right, float& bottom)
@@ -22,6 +20,15 @@ void CMushroom::GetBoundingBox(float& left, float& top, float& right, float& bot
 
 void CMushroom::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
+	vy += ay * dt;
+	vx += ax * dt;
+	if (y <= minY)
+	{
+		SetState(MUSHROOM_STATE_RUN);
+	}
+
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
 
 void CMushroom::Render()
@@ -32,4 +39,37 @@ void CMushroom::Render()
 
 	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
 	//RenderBoundingBox();
+}
+
+void CMushroom::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case MUSHROOM_STATE_UP:
+		vy = -MUSHROOM_SPEED_UP;
+		break;
+	case MUSHROOM_STATE_RUN:
+		vx = MUSHROOM_RUN_SPEED;
+		ay = MUSHROOM_GRAVITY;
+		break;
+	}
+}
+
+void CMushroom::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void CMushroom::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	if (e->ny != 0)
+	{
+		vy = 0;
+	}
+	else if (e->nx != 0)
+	{
+		vx = -vx;
+	}
 }
